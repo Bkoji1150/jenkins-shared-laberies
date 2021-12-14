b44348d67f944051818337fae70f4371668ff6fd
kojibello <kojibello@MacBook-Pro.local>
2021-12-14 13:49:51 -0800

    adding var folder

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(Map config) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
-    sh "echo Hello world"
+    sh "echo Hello world, ${config.name}"
 }
