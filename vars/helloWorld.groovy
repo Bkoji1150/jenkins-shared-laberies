b36565f6c6912c341920eec392cc8570034e66b4
kojibello <kojibello@MacBook-Pro.local>
2021-12-14 14:36:12 -0800

    adding variable

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,8 @@
-def call(String name,String dayofweek) {
-    // you can call any valid step functions from your code, just like you can from Pipeline scripts
-    sh "echo Hello ${name} today is ${dayofweek}"
+// def call(String name,String dayofweek) {
+//     // you can call any valid step functions from your code, just like you can from Pipeline scripts
+//     sh "echo Hello ${name} today is ${dayofweek}"
+// }
+
+def call(map config = [:]) {
+    sh "echo hello ${config.name} today is ${config.day_of_the_week}"
 }
