c940f1e1de8961d4c84a5a1a822b87b1ff8c63e2
Bkoji1150 <kojibello058@gmail.com>
2022-06-08 00:07:11 -0700

    added

--- vars/kojitechsTerraform.groovy
+++ vars/kojitechsTerraform.groovy
@@ -1,90 +1,120 @@
-// Define variable
-// def account = params.ENVIRONMENT
-def call(StringGitHubRepository ){
-        pipeline {
-            agent any
-            tools {
-            terraform 'terraform'
-            }
-            parameters { 
-            choice(name: 'ENVIRONMENT', choices: ['', 'prod', 'sbx', 'dev'], description: "SELECT THE ACCOUNT YOU'D LIKE TO DEPLOY TO.")
-            choice(name: 'ACTION', choices: ['', 'plan-apply', 'destroy'], description: 'Select action, BECAREFUL IF YOU SELECT DESTROY TO PROD')
-            
-            }
-            stages{
-                
-                stage('clean workspaces -----------') { 
-                    steps {
-                    cleanWs()
-                    sh 'env'
-                    } //steps
-                }  //stage
 
-                stage('Git checkout') {
-                steps{
-                        git "${GitHubRepository}"
-                        sh 'pwd' 
-                        sh 'ls -l'
-                    }
-                    
+def call(String repoUrl) {
+    pipeline {
+        agent any
+        tools {
+        terraform 'terraform'
+        }
+        parameters { 
+        choice(name: 'ENVIRONMENT', choices: ['', 'prod', 'sbx', 'dev'], description: "SELECT THE ACCOUNT YOU'D LIKE TO DEPLOY TO.")
+        choice(name: 'ACTION', choices: ['', 'plan-apply', 'destroy'], description: 'Select action, BECAREFUL IF YOU SELECT DESTROY TO PROD')
+        }
+        stages{
+            stage('Git checkout') {
+            steps{
+                   git branch: 'master',
+                       url: "${repoUrl}"
+                    sh """
+                        pwd
+                        ls -l
+                    """
                 }
-                stage('terraform Init') {
-                    steps{
-                        sh 'terraform init'
-                        sh 'terraform --version'
-                        sh 'ls -la'
-                    }
+            }
+            stage('TerraformInit'){
+                steps {
+                        sh """
+                            rm -rf .terraform 
+                            terraform init -upgrade=true
+                            echo \$PWD
+                            whoami
+                        """
                 }
-                stage('terraform create workspace') {
-                    steps{
-                        sh "terraform workspace select ${account}"
-                    }
-                }
-                stage('terraform plan') {
-                    steps{
-                        sh "terraform plan -var-file='${account}.tfvars' -refresh=true -lock=false -no-color  -out='${account}.plan' "
-                    }
-                }
-                stage('Confirm your action') {
-                    steps {
+            }
+        stage('Create Terraform workspace'){
+                steps {
                         script {
-                            def userInput = input(id: 'confirm', message: params.ACTION + '?', parameters: [ [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Apply terraform', name: 'confirm'] ])
+                            try {
+                                sh "terraform workspace select ${params.ENVIRONMENT}"
+                            } catch (Exception e) {
+                                echo "Error occurred: ${e}"
+                                sh """
+                                    terraform workspace new ${params.ENVIRONMENT}
+                                    terraform workspace select ${params.ENVIRONMENT}
+                                """
+                            }
+                
                         }
+            }
+        }
+            stage('Terraform plan'){
+                steps {
+                        script {
+                            try{
+                                sh "terraform plan -var-file='${params.ENVIRONMENT}.tfvars' -refresh=true -lock=false -no-color -out='${params.ENVIRONMENT}.plan'"
+                            } catch (Exception e){
+                                echo "Error occurred: ${e}"
+                                sh "terraform plan -refresh=true -lock=false -no-color -out='${params.ENVIRONMENT}.plan'"
+                            }
+                        }
+                
+                }
+            }
+            stage('Confirm your action') {
+                steps {
+                    script {
+                        def userInput = input(id: 'confirm', message: params.ACTION + '?', parameters: [ [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Apply terraform', name: 'confirm'] ])
                     }
                 }
-            stage('Terraform apply or destroy ----------------') {
-                    steps {
-                    sh 'echo "continue"'
-                    script{  
-                        if (params.ACTION == "destroy"){
-                                sh 'echo "llego" + params.ACTION'   
-                                sh "terraform destroy -var-file=${account}.tfvars -no-color -auto-approve"
-                        } else {
-                                sh ' echo  "llego" + params.ACTION'                 
-                                sh "terraform apply ${account}.plan"
-                        }  // if
-
-                    }
-                    } //steps
-                }  //stage
-        }
-        post {
-
-            success {
-            slackSend botUser: true, channel: 'jenkins_notification', color: 'good',
-            message: "The pipeline ${currentBuild.fullDisplayName} completed successfully.\nMore info ${env.BUILD_URL}\nLogin to ${account} and confirm.", 
-            teamDomain: 'slack', tokenCredentialId: 'slack'
             }
-            failure {
-            slackSend botUser: true, channel: 'jenkins_notification', color: 'danger',
-            message: "The pipeline ${currentBuild.fullDisplayName} completed with failure.\nError in ${env.BUILD_URL}", 
-            teamDomain: 'slack', tokenCredentialId: 'slack'
-            }
-            cleanup {
-            cleanWs()
-            }
+        stage('Terraform apply or destroy ----------------') {
+                steps {
+                sh 'echo "continue"'
+                script{  
+                    if (params.ACTION == "destroy"){
+                        script {
+                            try {
+                                sh """
+                                    echo "llego" + params.ACTION
+                                    terraform destroy -var-file=${params.ENVIRONMENT}.tfvars -no-color -auto-approve
+                                """
+                            } catch (Exception e){
+                                echo "Error occurred: ${e}"
+                                sh "terraform destroy -no-color -auto-approve"
+                            }
+                        }
+                        
+                }else {
+                            sh"""
+                                echo  "llego" + params.ACTION
+                                terraform apply ${params.ENVIRONMENT}.plan
+                            """ 
+                    }  // if
 
+                }
+                } //steps
+            }  //stage
+    }
+    post {
+
+        success {
+        slackSend botUser: true, channel: 'jenkins_notification', color: 'good',
+        message: "${params.ACTION} with ${currentBuild.fullDisplayName} completed successfully.\nMore info ${env.BUILD_URL}\nLogin to ${params.ENVIRONMENT} and confirm.", 
+        teamDomain: 'slack', tokenCredentialId: 'slack'
         }
+        failure {
+        slackSend botUser: true, channel: 'jenkins_notification', color: 'danger',
+        message: "Your Terraform ${params.ACTION} with ${currentBuild.fullDisplayName} got failed.", 
+        teamDomain: 'slack', tokenCredentialId: 'slack'
         }
-}
-    
+        aborted {
+        slackSend botUser: true, channel: 'jenkins_notification', color: 'hex',
+        message: "Your Terraform ${params.ACTION} with ${currentBuild.fullDisplayName} got aborted.\n${env.BUILD_URL}", 
+        teamDomain: 'slack', tokenCredentialId: 'slack'
+        }
+        cleanup {
+        cleanWs()
+        }
+
+    }
+    }
+}    
