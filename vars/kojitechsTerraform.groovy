75b4bbb181368e51953bc5661663e1dfe1d849a6
kojitechs <85324828+Bkoji1150@users.noreply.github.com>
2022-09-12 13:49:49 -0700

    Update kojitechsTerraform.groovy

--- vars/kojitechsTerraform.groovy
+++ vars/kojitechsTerraform.groovy
@@ -6,8 +6,8 @@ def call() {
             terraform 'terraform'
         }
         parameters { 
-            choice(name: 'ENVIRONMENT', choices: ['', 'prod', 'sbx', 'shared'], description: "SELECT THE ACCOUNT YOU'D LIKE TO DEPLOY TO.")
-            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Select action, BECAREFUL IF YOU SELECT DESTROY TO PROD')
+            choice(name: 'ENVIRONMENT', choices: ['sbx', 'prod', 'sbx', 'shared'], description: "SELECT THE ACCOUNT YOU'D LIKE TO DEPLOY TO.")
+            choice(name: 'ACTION', choices: ['apply', 'apply', 'destroy'], description: 'Select action, BECAREFUL IF YOU SELECT DESTROY TO PROD')
         }
         stages{    
             stage('Git checkout') {
